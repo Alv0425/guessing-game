ef28c7c10cf960d7f027dc72ab54b7b349413098
Alv0425 <vilejshikova@gmail.com>
2023-09-17 14:16:42 +0300

    feat: add GuessingGame constructor and methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,22 +1,27 @@
 class GuessingGame {
     constructor() {
-
+        this.min = 0;
+        this.max = 0;
+        this.number = 0;
     }
 
-    setRange(min, max) {
-
+    setRange(min, max){
+        this.min = min;
+        this.max = max;
     }
 
-    guess() {
-
+    guess(){
+        return this.number = Math.round((this.max - this.min)/2) + this.min;
     }
 
-    lower() {
-
+    lower(){
+        this.min = this.min;
+        this.max = this.number;
     }
-
-    greater() {
-
+    
+    greater(){
+        this.min = this.number;
+        this.max = this.max;
     }
 }
 
